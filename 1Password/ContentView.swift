73629225930f9eabbe54dbb154e85a5f4f3a8b64
Himanshu Kasera <hkasera23@fed.idserve.net>
2023-11-15 10:00:17 +0100

    Initial Commit

---
+++ 1Password/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  1Password
+//
+//  Created by Himanshu Kasera on 15/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 1Password/_PasswordApp.swift
@@ -0,0 +1,17 @@
+//
+//  _PasswordApp.swift
+//  1Password
+//
+//  Created by Himanshu Kasera on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct _PasswordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
